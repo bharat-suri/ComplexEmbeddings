f9fd76dd22ab5f4fcccc31e61b6a9a86e6de9ff0
bharat.suri <bsuri97@gmail.com>
2018-06-28 20:03:30 +0530

    Added docs

--- src/lstm.py
+++ src/lstm.py
@@ -19,14 +19,28 @@ logging.root.level = logging.INFO
 wv = None
 
 def load_model(savedModel):
+	"""
+	Load the saved model containing
+	pre-trained embeddings. I deleted
+	the model while just keeping the
+	vectors to save memory.
+	"""
 	global wv
 	model = FastText.load(savedModel)
 	wv = model.wv
 	del model
 
 def load_mappings(descriptions):
+	"""
+	Input to an LSTM must be a 3D tensor. For that, I
+	am reading all the descriptions line by line and
+	storing their embeddings row wise into a numpy
+	array of 3 dimensions and shape (n, 500, 300).
+	"""
 	global wv
 	count = 0
+
+	# Fixed shape for the input layer of the lstm network.
 	shape = (500, 300)
 	entities = []
 	abstracts = []
@@ -35,13 +49,20 @@ def load_mappings(descriptions):
 		for line in file:
 			if count % 5000 == 0:
 				logging.info('reading resource # {0}'.format(count))
-			jsonline = json.loads(line)		
+			jsonline = json.loads(line)
+
+			# Resource is extracted
 			ent = [_ for _ in jsonline.keys()][0]
+
+			# Description is extracted and split into tokens
 			desc = jsonline[ent].split()
 			try:
+				# Using a temporary array to pad the original embedding, can be replaced by np.pad()
 				t = np.zeros(shape, dtype=np.float32)
 				v = np.array(wv.get_vector(ent), dtype=np.float32)
 				r = np.array(list(map(lambda x: wv.get_vector(x), desc)), dtype=np.float32)
+
+				# Padding the array to a fixed shape.
 				t[:r.shape[0], :r.shape[1]] = r
 				entities.append(v)
 				abstracts.append(t)
@@ -50,6 +71,8 @@ def load_mappings(descriptions):
 				continue
 	del wv
 	logging.info('resources read into np stack of length : {0}'.format(count))
+
+	# Currently the process is killed here because of memory pressure.
 	entities = np.stack(entities)
 	abstracts = np.stack(abstracts)
 	logging.info('saving entity embeddings to data/entity.npy')
